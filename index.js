9b31fad522e39ecd7e739d1b5aedb8d202fb3e56
Nithish <nithishmani79@gmail.com>
2025-08-09 14:55:45 +0530

    Js page

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const closeMenu = document.getElementById("closeMenu");
   const searchBtn = document.getElementById("searchBtn");
   const searchInput = document.getElementById("searchInput");
+  const searchCategory = document.getElementById("searchCategory");
 
   // Show/Hide sidebar
   toggleMenu.addEventListener("click", () => {
